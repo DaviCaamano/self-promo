12a3b443a8f5a7ddd8a3667552032f994ca6643f
Davi Caamano <davi.caamano@shiftpixy.com>
2023-10-29 22:34:07 -0400

    feat: selfie page responsive

--- src/components/landing/selfie/JobTitle.tsx
+++ src/components/landing/selfie/JobTitle.tsx
@@ -3,20 +3,17 @@ import styles from './job-title.module.scss';
 export const JobTitle = () => (
   <>
     <div className={styles.jobTitle}>
-      <div className={'ml-14'}>[<InlineSpacerLg />Web]</div>
+      <div className={'ml-14'}>[Web]</div>
       <div className={'ml-14'}>
-        [<InlineSpacer />
-        Mobile]
+        [Mobile]
       </div>
       <div className={'ml-14 mt-[2px]'}>
-        [<InlineSpacer />
-        Fullstack]
+        [Fullstack]
       </div>
     </div>
     <div className={styles.jobTitleInner}>
       <div className={'ml-14'}>[Web]</div>
-      <div className={'ml-14'}>
-        [<InlineSpacer />Mobile]
+      <div className={'ml-14'}>[Mobile]
       </div>
       <div className={'ml-14 mt-[2px]'}>
         [<InlineSpacer />
@@ -26,5 +23,5 @@ export const JobTitle = () => (
   </>
 );
 
-const InlineSpacer = () => <span className={'mr-[1px] inline-block '} />;
+const InlineSpacer = () => <span className={'mr-[3px] inline-block '} />;
 const InlineSpacerLg = () => <span className={'mr-[4px] inline-block '} />;
